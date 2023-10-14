d35d0f8839046e292db3b68e839dae8b2a2bba60
Your Name <you@example.com>
2023-10-15 00:30:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+
+
+// ---------------- <juft yoki toq start> ----------------------------------
+
+// let a = prompt ("Son kirit");
+
+// if (a % 2 == 0) {
+//     console.log(`${a} juft son `);
+// } else {
+//     console.log(`${a} toq son `);
+// }
+// ---------------- <juft yoki toq end> ----------------------------------
+
+
+
+
+// ---------------- <teskari son chiqarish start> ----------------------------------
+
+// let a = prompt ("Son kirit"); 
+// let b = a % 10;
+// let c = (a - b)/10;
+// let d = b * 10 + c;
+// console.log(d);
+
+// ---------------- <teskari son end> ----------------------------------
+
+
+
+// ---------------- <sodda kankulatr start> ----------------------------------
+
+// let a = prompt("birinchi son kirit")
+// let amal = prompt("amalni kirit")
+// let b = prompt("ikkinchi son kirit")
+
+// if (amal == "*") {
+//     console.log(a * b);
+// }else if (amal == "/") {
+//     console.log(a / b);
+// }else if(amal == "+") { 
+//     console.log(+a + +b);
+    
+// }else if (amal == "-") {
+//     console.log(a - b);
+// }
+// ---------------- <sodda kankulatr end> ----------------------------------
+
+
+// let a = prompt ("son kirit");
+
+// if (a / 5) {
+//     console.log(`${a} 5ga bo'linadi`);
+// }else {
+//     console.log(`${a} 5 bo'linmaydi`);
+// }
+
+
+// ---------------- <5ga yaqin son start> ----------------------------------
+
+
+// const a = parseInt(prompt("a ni kiriting:"));
+// const b = parseInt(prompt("b ni kiriting:"));
+
+// const checkA = a > 5 ? a - 5 : 5 - a;
+// const checkB = b > 5 ? b - 5 : 5 - b;
+
+// if (checkA < checkB) {
+//   alert(`${a} 5 ga yaqin`);
+// } else if (checkA > checkB) {
+//   alert(`${b} 5 ga yaqin`);
+// } else {
+//   alert(`${a} va ${b} 5 ga teng yaqin`);
+// }
+
+// ---------------- <5ga yaqin son end> ----------------------------------
